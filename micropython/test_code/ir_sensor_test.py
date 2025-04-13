b5440f9ec8730847187c64ad9e2d30264957be46
EetuHolmi <eetu.holmi@gmail.com>
2025-04-13 18:09:11 +0300

    added ir sensor test code

---
+++ micropython/test_code/ir_sensor_test.py
@@ -0,0 +1,14 @@
+from machine import Pin
+import utime
+
+# define led and ir_sensor pins
+led = Pin(16, Pin.OUT)
+ir_sensor = Pin(15, Pin.IN)
+
+# infinite loop to check sensor and change led
+while True:
+    print(ir_sensor.value())
+    if ir_sensor.value() == 1:
+        led.value(0)
+    else:
+        led.value(1) 
